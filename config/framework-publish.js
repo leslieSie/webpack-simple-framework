1ad26ab7f072a7b86bac247f9a9c5d992b502a5d
leslieSie <18826220848@163.com>
2019-06-10 23:50:45 +0800

    update

--- config/framework-publish.js
+++ config/framework-publish.js
@@ -1,7 +1,21 @@
 const fs = require('fs');
 const shell = require('shelljs');
+const colors = require('colors');
+let bundlePackage = '';
 shell.cd('build');
-/* shell.exec('npm login', function(code, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-}) */
+shell.exec('yarn -v', function(code, stdout, stderr) {
+    if (code != 0) {
+        console.log(stderr.red);
+        return false;
+    }
+    bundlePackage = 'yarn'
+    console.log('支持yarn打包!'.magenta);
+});
+
+shell.exec(`${bundlePackage} add -g npm-cli-login`, function(code, stdout, stderr) {
+    if (code != 0) {
+        console.log(stderr.red);
+        return false;
+    }
+    console.log('npm-cli-login 安装完成'.green);
+});
