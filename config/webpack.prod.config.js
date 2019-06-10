13a858025f825b2b1d8d5f137d96b46f065f8d30
leslieSie <18826220848@163.com>
2019-06-10 13:58:29 +0800

    添加了prod打包的异常处理和对README.md文件的检测

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,117 +1,101 @@
-const webpackMerge = require('webpack-merge');
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const prodConfig = require('./prod.config.js');
-const copyWebpackPlugin = require('copy-webpack-plugin');
-const baseConfig = require('./webpack.base.config.js');
+const webpackMerge = require("webpack-merge");
+const path = require("path");
+const fs = require("fs");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const prodConfig = require("./prod.config.js");
+const copyWebpackPlugin = require("copy-webpack-plugin");
+const baseConfig = require("./webpack.base.config.js");
 const {
-    absPath,
-    file_exit,
-    dir_create
-} = require('./utils.js');
+  absPath,
+  file_exit,
+  dir_create
+} = require("./utils.js");
 
 let copyFiles = [{
-    from: 'package.json'
-}, {
-    from: 'README.md'
-}, {
-    from: 'LICENSE'
-}];
+    from: "package.json"
+  },
+  {
+    from: "./src/README.md",
+    to: 'README.md'
+  },
+  {
+    from: "LICENSE"
+  }
+];
 
-let folder_exits = file_exit(path.join(__dirname, '../', 'build'));
+let folder_exits = file_exit(path.join(__dirname, "../", "build"));
 
-/* module.exports = webpackMerge(baseConfig, {
-    entry: [absPath(`src/${prodConfig.entryMainFile || 'index.js'}`)],
-    devtool: false,
-    mode: 'production',
-    output: {
-        path: absPath('build'),
-        filename: `${prodConfig.bundleFile || 'main.js'}`,
-        libraryExport: 'default',
-        libraryTarget: 'commonjs'
-    },
-    module: {
-        rules: [{
+async function dealBuild() {
+  let process = new Promise((resolve, reject) => {
+      if (!folder_exits) {
+        dir_create(path.join(__dirname, "../", "build"), err => {
+          if (err) {
+            console.error(err);
+          }
+          console.log("新建打包目录");
+        });
+      }
+      resolve(true);
+    })
+    .then(data => {
+      console.log("处理生成文件");
+      let isShowSource = prodConfig.showSource || false;
+      let isExample = prodConfig.packageExample || false;
+      // 检测打包的框架中是否存在README.md文件
+      if (!fs.existsSync(absPath('src/README.md'))) {
+        throw '打包的框架需要有README.md文件';
+      }
+      if (isExample) {
+        copyFiles.push({
+          from: "view",
+          to: "example"
+        });
+      }
+      if (isShowSource) {
+        copyFiles.push({
+          from: "src",
+          to: prodConfig.outputSourceDirectory || "source"
+        });
+      }
+      return true;
+    })
+    .then(data => {
+      return webpackMerge(baseConfig, {
+        entry: [absPath(`src/${prodConfig.entryMainFile || "index.js"}`)],
+        devtool: false,
+        mode: "production",
+        output: {
+          path: absPath("build"),
+          filename: `${prodConfig.bundleFile || "main.js"}`,
+          libraryExport: "default",
+          libraryTarget: "commonjs"
+        },
+        module: {
+          rules: [{
             test: /\.js$/,
             exclude: /node_modules/,
             use: [{
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
-                }
+              loader: "babel-loader",
+              query: {
+                presets: ["es2015"]
+              }
             }]
-        }, ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new copyWebpackPlugin(copyFiles)
-    ]
-}); */
-async function dealBuild() {
-    let process = new Promise((resolve, reject) => {
-        if (!folder_exits) {
-            dir_create(path.join(__dirname, '../', 'build'), (err) => {
-                if (err) {
-                    console.error(err);
-                }
-                console.log('新建打包目录');
-            });
-        }
-        resolve(true);
-    }).then(data => {
-        console.log('处理生成文件')
-        console.log(prodConfig);
-        let isShowSource = prodConfig.showSource || false;
-        let isExample = prodConfig.packageExample || false;
-
-        if (isExample) {
-            copyFiles.push({
-                from: 'view',
-                to: 'example'
-            });
-        }
-        if (isShowSource) {
-            copyFiles.push({
-                from: 'src',
-                to: prodConfig.outputSourceDirectory || 'source'
-            });
-        }
-        return true;
-    }).then(data => {
-        return webpackMerge(baseConfig, {
-            entry: [absPath(`src/${prodConfig.entryMainFile || 'index.js'}`)],
-            devtool: false,
-            mode: 'production',
-            output: {
-                path: absPath('build'),
-                filename: `${prodConfig.bundleFile || 'main.js'}`,
-                libraryExport: 'default',
-                libraryTarget: 'commonjs'
-            },
-            module: {
-                rules: [{
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: [{
-                        loader: 'babel-loader',
-                        query: {
-                            presets: ['es2015']
-                        }
-                    }]
-                }, ]
-            },
-            plugins: [
-                new CleanWebpackPlugin(),
-                new copyWebpackPlugin(copyFiles)
-            ]
-        });
-    })
-    let exportData = await process;
-    return exportData;
-};
-
-module.exports = async function() {
-    let config = await dealBuild();
-    console.log(config);
-    return config;
+          }]
+        },
+        plugins: [new CleanWebpackPlugin(), new copyWebpackPlugin(copyFiles)]
+      });
+    }).catch(err_msg => {
+      console.error(`打包错误：${err_msg}`);
+      return 'error';
+    });
+  let exportData = await process;
+  return exportData;
 }
+
+module.exports = async function () {
+  let config = await dealBuild();
+  if (config === 'error') {
+    return {}
+  }
+  return config;
+};
---
+++ src/README.md
@@ -0,0 +1,98 @@
+# webpack-simple-framework
+
+webpack-simple-framework 一个基于 webpack 构建的脚手架工具，用于对 JS 框架进行打包处理使用，**面向轮子打包使用，非直接构建 web 应用**。
+
+<br />
+
+<br />
+
+<br />
+
+### webpack-simple-framework 与 vue-cli 的差异：
+
+> 1. webpack-simple-framework 相较于 vue-cli 比较简单，没有太过复杂的框架配置，方便上手与对脚手架的修改
+> 2. webpack-simple-framework 将演示的代码与最后打包出来的代码分离出来，而且 vue-cli 只针对项目代码，也即是 vue-cli 开发和生产模式下打包的代码是一样的。
+> 3. webpack-simple-framework 支持命令行发布代码到 npm 上（实现中）:worried:
+> 4. webpack-simple-framework 支持对 eslint 一键自动修复代码编码风格
+> 5. webpack-simple-framework 支持单元测试，保障发布代码质量正常运行
+> 6. 未来支持 vue 和 react(虽然目前只支持 vue) :worried:
+
+<br />
+
+<br />
+
+### 安装运行方法
+
+> git clone https://github.com/leslieSie/webpack-simple-framework.git
+>
+> yarn install (推荐) || npm install
+>
+> yarn dev || npm run dev
+
+<br />
+
+<br />
+
+<br />
+
+### scripts 的用法
+
+**dev** : 用来开启开发模式，自动运行开发服务器，默认网址：http:localhost:8888
+
+可以通过在 config 文件夹下面的 dev.config.js 文件去做修改。
+
+**build** : 用于对要编写的框架进行打包成最终发布效果的,生成的地址在 build 文件夹下面
+
+**lint** : 用于对代码进行 eslint 的检测和修复，相当执行 lint:dev 和 lint:prod 两条命令
+
+~~**lint:dev** : 对 view 目录下面的代码进行风格检测修复~~
+
+~~**lint:prod** : 对 src 目录下面的代码进行风格化修复~~
+
+<br />
+
+<br />
+
+<br />
+
+##### 目录结构说明
+
+![目录文档结图](./public/directory.png)
+
+**build 目录** : 用于存放最终打包发布的文件目录，一般在这个文件目录进行修改，如果安装之后没有 build 文件的话，执行 yarn build 就会自动生成产生。
+
+**config 目录** : 存放 webpack 打包配置文件
+
+**dist 目录** : dev server 的指向目录，用于开发模式下面的调试，不在上面进行修改
+
+**public 目录** : 存放公共的资源文件，一般存放图片等静态资源
+
+**src 目录** : 最终要打包的框架的源码
+
+**view 目录** : 开发模式对应测试代码存放的目录，区别于**src 目录**,view 目录执行 yarn build 的情况下，不会对 view 目录下面的代码进行打包，该目录下面的代码仅用于在开发模式下的调试使用。
+
+<br />
+
+<br />
+
+<br />
+
+##### 项目其他参与者和贡献者
+
+<a href="https://github.com/fengxianqi" target="_blank">
+
+​	<img style="border-radius:50%;border:1px solid gray" width="100" height="100"  src="https://avatars1.githubusercontent.com/u/15716381?s=460&v=4" />
+
+</a>
+
+<br />
+
+<br />
+
+<br />
+
+### 扶持作者
+
+开源不易，一分钱也是钱，希望得到各位大佬们的支持，你的支持是我前进的动力。
+
+![微信支付码](./public/alipay_qrcode.jpg)
