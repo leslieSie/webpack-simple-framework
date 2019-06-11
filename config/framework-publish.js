4e1e8f07fc9b074cf53411566b47da61fa60b864
leslieSie <18826220848@163.com>
2019-06-11 11:33:08 +0800

    实现一个简单的npm login 代码层方案

--- config/framework-publish.js
+++ config/framework-publish.js
@@ -1,21 +1,25 @@
 const fs = require('fs');
 const shell = require('shelljs');
 const colors = require('colors');
+const npmLogin = require('npm-cli-login');
 let bundlePackage = '';
 shell.cd('build');
-shell.exec('yarn -v', function(code, stdout, stderr) {
-    if (code != 0) {
-        console.log(stderr.red);
-        return false;
-    }
-    bundlePackage = 'yarn'
-    console.log('支持yarn打包!'.magenta);
-});
+npmLogin('XXXXXXXXX','XXXXXXXXX','XXXXXXXXX');
+/* shell.exec('yarn -v', function (code, stdout, stderr) {
+  if (code != 0) {
+    console.log(stderr.red);
+    return false;
+  }
+  bundlePackage = 'yarn'
+  console.log('支持yarn打包!'.magenta);
+  console.log(bundlePackage);
 
-shell.exec(`${bundlePackage} add -g npm-cli-login`, function(code, stdout, stderr) {
-    if (code != 0) {
-        console.log(stderr.red);
-        return false;
-    }
-    console.log('npm-cli-login 安装完成'.green);
 });
+shell.exec(`npm install npm-cli-login -g`, function (code, stdout, stderr) {
+  if (code != 0) {
+    console.log(stderr.red);
+    return false;
+  }
+  console.log('npm-cli-login 安装完成'.green);
+});
+ */
