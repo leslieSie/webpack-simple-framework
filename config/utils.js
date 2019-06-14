952a5d933f9c1e00355a3edd210cfd777d2c2839
leslieSie <18826220848@163.com>
2019-06-14 13:03:23 +0800

    提供文件类型和文件生成的公共方法，检测和生成文件存储目录

--- config/utils.js
+++ config/utils.js
@@ -1,32 +1,47 @@
 let path = require("path");
 let fs = require("fs");
+let mkdirp = require('mkdirp');
 
 // generate file address
-let absPath = function(dir) {
+let absPath = function (dir) {
   return path.join(__dirname, "../", dir);
 };
 
 // judge the file is exit
-let fileExit = function(absPath) {
+let fileExit = function (absPath) {
   return fs.existsSync(absPath);
 };
 
 // create new directory
-let dirCreate = function(absPath, fn) {
-  fs.mkdir(absPath, fn);
+let dirCreate = function (absPath, fn) {
+  mkdirp(absPath, err => {
+    if (err) {
+      console.log(err.red);
+      return false;
+    }
+    fn();
+  });
 };
 
 //judge the dataType
-let dataType = function(data) {};
+let dataType = function (data) {
+  let tmpType = Object.prototype.toString.call(data);
+  let sliceString = tmpType.slice(1, tmpType.length - 2);
+  return sliceString.split(' ').reverse()[0];
+};
 
 //create file on absolute path.if the path is not exits,this function can create the file whitch you dictate
-let createFile = function(absPath = "", fn) {};
+let createFile = function (absPath = "", fn) {};
 
 // delete file
-let deleteFile = function(absPath = "", params, fn) {};
+let deleteFile = function (absPath = "", params, fn) {};
 
 //store to file
-let store2File = function(absPath = "", writeMsg) {};
+let store2File = function (absPath = "", writeMsg) {
+ /*  fs.createWriteStream(
+
+  ) */
+};
 
 const global_exclude = [/node_modules/, absPath("build")];
 
