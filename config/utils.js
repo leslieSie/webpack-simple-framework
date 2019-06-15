137af761f3dc594fb68ff0b781a4bfdbd905a2ea
leslieSie <18826220848@163.com>
2019-06-16 01:25:43 +0800

    优化测试新增文件模块，解析路径模块，数据类型判断模块

--- config/utils.js
+++ config/utils.js
@@ -1,57 +1,145 @@
 let path = require("path");
 let fs = require("fs");
 let mkdirp = require('mkdirp');
+const colors = require('colors');
 
 // generate file address
-let absPath = function (dir) {
-  return path.join(__dirname, "../", dir);
+let absPath = function(dir) {
+    return path.join(__dirname, "../", dir);
 };
 
-// judge the file is exit
-let fileExit = function (absPath) {
-  return fs.existsSync(absPath);
+// judge the file is exist
+let fileExist = function(absPath) {
+    return fs.existsSync(absPath);
 };
 
-// create new directory
-let dirCreate = function (absPath, fn) {
-  mkdirp(absPath, err => {
-    if (err) {
-      console.log(err.red);
-      return false;
+// create new directory,if the directory exist that the function return the string which is 'the directory is exist'
+let dirCreate = function(absPath, fn) {
+    let directoryIsExist = fileExist(absPath);
+    if (directoryIsExist) {
+        console.log('要创建的文件目录已经存在'.red);
+        return {
+            status: 'exist'
+        }
+    } else {
+        mkdirp(absPath, err => {
+            if (err) {
+                console.log(err.red);
+                return {
+                    status: 'error'
+                }
+            }
+            if (dataType(fn) == 'Function') {
+                fn();
+            }
+        });
+        return {
+            status: 'success'
+        }
     }
-    fn();
-  });
 };
 
-//judge the dataType
-let dataType = function (data) {
-  let tmpType = Object.prototype.toString.call(data);
-  let sliceString = tmpType.slice(1, tmpType.length - 2);
-  return sliceString.split(' ').reverse()[0];
+//judge the data type
+let dataType = function(data) {
+    let tmpType = Object.prototype.toString.call(data);
+    let sliceString = tmpType.slice(1, tmpType.length - 1);
+    return sliceString.split(' ').reverse()[0];
 };
 
-//create file on absolute path.if the path is not exits,this function can create the file whitch you dictate
-let createFile = function (absPath = "", fn) {};
+// get file name and directory name from absolute path
+let getFileMsg = function(absPath) {
+    let basename = path.basename(absPath);
+    let dirname = path.dirname(absPath);
+    return {
+        filename: basename,
+        dirname: dirname
+    }
+};
+
+// create file on specified path,If the path mean directory,will return error message.If the path whitch is exist file,return a message to tell developer the file is exist.otherwise,create files depend on specifiedPath param.create succes run cb params.by the way,fn must Function type.
+let createFile = function(specifiedPath = "", cb) {
+    new Promise((resolve, reject) => {
+            fs.stat(specifiedPath, (err, stats) => {
+                if (err) {
+                    resolve();
+                    return false;
+                }
+                if (stats.isDirectory()) {
+                    reject({
+                        msg: '传入的路径不能为目录路径'
+                    })
+                }
+                if (stats.isFile()) {
+                    reject({
+                        msg: '文件已经存在'
+                    });
+                }
+            })
+        })
+        .then(data => {
+            let fileMsgs = getFileMsg(specifiedPath);
+            mkdirp(fileMsgs.dirname, err => {
+                if (err) {
+                    console.log('文件创建失败'.red);
+                    return false;
+                }
+                fs.writeFile(path.join(fileMsgs.dirname, fileMsgs.filename), {}, (err) => {
+                    if (err) {
+                        console.log(err.red);
+                        return false;
+                    }
+                });
+                if (dataType(cb) == 'Function') {
+                    cb();
+                }
+            });
+        })
+        .catch(errObj => {
+            console.log(errObj.msg.red)
+        })
+
+};
 
 // delete file
-let deleteFile = function (absPath = "", params, fn) {};
+let delFiles = function(absPaths, params, fn) {
+    switch (dataType(absPaths)) {
+        case 'String':
+            let isExist = fileExist(absPaths);
+            if (isExist) {
+                fs.unlink(absPaths, (err) => {
+                    if (err) {
+                        console.log(err);
+                        return false;
+                    }
+                    console.log('文件删除成功!');
+                    return true;
+                });
+            } else {
+
+            }
+            break;
+        case 'Function':
+            break;
+    }
+};
 
 //store to file
-let store2File = function (absPath = "", writeMsg) {
- /*  fs.createWriteStream(
+let store2File = function(absPath = "", writeMsg) {
+    /*  fs.createWriteStream(
 
-  ) */
+     ) */
 };
 
 const global_exclude = [/node_modules/, absPath("build")];
 
 module.exports = {
-  absPath,
-  fileExit,
-  dirCreate,
-  global_exclude,
-  dataType,
-  createFile,
-  deleteFile,
-  store2File
+    absPath,
+    fileExist,
+    dirCreate,
+    global_exclude,
+    dataType,
+    createFile,
+    delFiles,
+    store2File,
+    getFileMsg
 };
