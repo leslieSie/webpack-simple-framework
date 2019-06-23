9e00e239cc70e6f0b724c8b2830cf6020b177bc4
leslieSie <18826220848@163.com>
2019-06-23 16:50:06 +0800

    删除冗余代码注释

--- config/framework-publish.js
+++ config/framework-publish.js
@@ -8,44 +8,21 @@ var client = new RegClient();
 
 var uri = "https://registry.npmjs.org/npm"
 client.get(uri, {
-        username: userMsg.username,
-        password: userMsg.password,
-        email: userMsg.email
-    }, function(error, data, raw, res) {
-        console.log('npm login success'.green);
-        readFromFile(absPath('build/package.json'), {
-                type: 'json'
-            }, () => {
-                conso.e.log(arguments);
-            })
-            // console.log(res);
-            // error is an error if there was a problem.
-            // data is the parsed data object
-            // raw is the json string
-            // res is the response from couch
-    })
-    /* async function test() {
-        // await npmLogin(userMsg.username, userMsg.password, userMsg.email);
-
+    username: userMsg.username,
+    password: userMsg.password,
+    email: userMsg.email
+}, function(error, data, raw, res) {
+    if (error) {
+        console.log(error.red);
+        return false;
     }
-    test(); */
-
-// console.log(1);
-/* shell.exec('yarn -v', function (code, stdout, stderr) {
-  if (code != 0) {
-    console.log(stderr.red);
-    return false;
-  }
-  bundlePackage = 'yarn'
-  console.log('支持yarn打包!'.magenta);
-  console.log(bundlePackage);
+    console.log('npm login success'.green);
+    readFromFile(absPath('build/package.json'), {
+        type: 'json'
+    }, (err, msg) => {
+        console.log(err);
+        console.log(typeof msg);
+        console.log(msg);
+    })
 
 });
-shell.exec(`npm install npm-cli-login -g`, function (code, stdout, stderr) {
-  if (code != 0) {
-    console.log(stderr.red);
-    return false;
-  }
-  console.log('npm-cli-login 安装完成'.green);
-});
- */
