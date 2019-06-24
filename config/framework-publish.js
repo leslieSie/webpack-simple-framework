547e2d5e1bbea8c92c84900d097b80fbc355ec85
leslieSie <18826220848@163.com>
2019-06-24 13:49:13 +0800

    update

--- config/framework-publish.js
+++ config/framework-publish.js
@@ -1,28 +1,44 @@
-const fs = require('fs');
-const shell = require('shelljs');
-const colors = require('colors');
-const RegClient = require('npm-registry-client');
-const userMsg = require('./npmUserLogin.js');
-const { readFromFile, absPath } = require('./utils.js');
+const fs = require("fs");
+const shell = require("shelljs");
+const colors = require("colors");
+const RegClient = require("npm-registry-client");
+const userMsg = require("./npmUserLogin.js");
+const { readFromFile, absPath, store2File } = require("./utils.js");
 var client = new RegClient();
 
-var uri = "https://registry.npmjs.org/npm"
+readFromFile(
+  absPath("build/package.json"),
+  {
+    type: "json"
+  },
+  (err, msg) => {
+    msg.version = "0.1.0";
+    store2File(
+      absPath("build/package.json"),
+      {
+        type: "json"
+      },
+      msg
+    );
+  }
+);
+/* var uri = "https://registry.npmjs.org/npm"
 client.get(uri, {
-    username: userMsg.username,
-    password: userMsg.password,
-    email: userMsg.email
+    username: userMsg.loginConfig.username,
+    password: userMsg.loginConfig.password,
+    email: userMsg.loginConfig.email
 }, function(error, data, raw, res) {
     if (error) {
         console.log(error.red);
         return false;
     }
     console.log('npm login success'.green);
-    readFromFile(absPath('build/package.json'), {
-        type: 'json'
-    }, (err, msg) => {
-        console.log(err);
-        console.log(typeof msg);
-        console.log(msg);
-    })
+    // readFromFile(absPath('build/package.json'), {
+    //     type: 'json'
+    // }, (err, msg) => {
+    //     console.log(err);
+    //     console.log(typeof msg);
+    //     console.log(msg.version);
+    // })
 
-});
+}); */
--- config/npmUserLogin.js
+++ config/npmUserLogin.js
@@ -1,5 +1,7 @@
 module.exports = {
-    username: 'lesliesie',
-    password: '1qaz@WSXxcy',
-    email: '2358407968@qq.com'
-}
+  loginConfig: { // 账号，密码，邮箱需要到Npm.com网站去注册
+    username: "lesliesie",
+    password: "1qaz@WSXxcy",
+    email: "2358407968@qq.com"
+  }
+};
