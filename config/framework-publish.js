8145c377f39217511ea1f255e16366c60dc70538
leslieSie <18826220848@163.com>
2019-06-26 13:13:36 +0800

    添加配置说明，完成发布的类型检测

--- config/framework-publish.js
+++ config/framework-publish.js
@@ -3,16 +3,63 @@ const shell = require("shelljs");
 const colors = require("colors");
 const RegClient = require("npm-registry-client");
 const userMsg = require("./npmUserLogin.js");
-const { readFromFile, absPath, store2File } = require("./utils.js");
+const { readFromFile, absPath, store2File, dataType } = require("./utils.js");
 var client = new RegClient();
 
-readFromFile(
+// 判断数据是否符合检测类型
+let detectCore = function(data) {
+  let returnStatus;
+  switch (dataType(data)) {
+    case "Null":
+    case "Undefined":
+      returnStatus = false;
+      break;
+
+    case "String":
+      if (Object.is(data, "")) {
+        returnStatus = false;
+      } else {
+        returnStatus = true;
+      }
+      break;
+
+    default:
+      returnStatus = true;
+      break;
+  }
+  return returnStatus;
+};
+
+// 检测入口
+let detectEntry = function() {
+  try {
+    for (let key in userMsg.loginConfig) {
+      let status = detectCore(userMsg.loginConfig[key]);
+     if(!status){
+       throw `you may not match setting on npmUserLogin.js,maybe property on userMsg.loginConfig.${key} is Empty`;
+     }
+    }
+  } catch (err) {
+    console.log(err.red);
+    return false;
+  }
+};
+
+// main
+(function() {
+  let isPass = detectEntry();
+  if(isPass){
+    console.log('continue');
+  }
+})();
+
+/* readFromFile(
   absPath("build/package.json"),
   {
     type: "json"
   },
   (err, msg) => {
-    msg.version = "0.1.0";
+    msg.version = "0.0.5";
     store2File(
       absPath("build/package.json"),
       {
@@ -21,7 +68,7 @@ readFromFile(
       msg
     );
   }
-);
+); */
 /* var uri = "https://registry.npmjs.org/npm"
 client.get(uri, {
     username: userMsg.loginConfig.username,
--- config/npmUserLogin.js
+++ config/npmUserLogin.js
@@ -1,7 +1,56 @@
 module.exports = {
-  loginConfig: { // 账号，密码，邮箱需要到Npm.com网站去注册
-    username: "lesliesie",
-    password: "1qaz@WSXxcy",
-    email: "2358407968@qq.com"
+  loginConfig: {
+    // 账号，密码，邮箱需要到https://www.npmjs.com/ 网站去注册
+    username: "leslieSie", // 必填
+    password: "", // 必填
+    email: "" // 必填
+  },
+  /**
+   * version接受两种类型的传参Object,Function，默认采用version Object
+   *  Function 指定需要返回的版本号，传入version函数的第一个参数为当前系统的版本号
+   *  函数返回的值将作为当前发布的版本号和下一次传入的参数。
+   *
+   *  Object version的参数配置对象
+   *    version.auto 接受参数为布尔类型，默认值为true,表示的是自动计算版本号，当static生效的时候，autoIncrement无效
+   *    version.static 接受类型为String类型，无默认值，当static不等于undefined或者空字符串的时候，该值表示手动自定版本号
+   *    version:autoIncrement 接受Number类型，默认值为1，当static生效的时候，autoIncrement无效
+   *
+   *  type支持三种类性质：structure,feature,bug
+   *  type用来表示修复版本的类型，配合 version.autoIncrement 定位发布版本的增量修改位置
+   *  每发布一个版本号，对于下一级的版本号都会清零
+   *  版本号的定义格式是：
+   *  {structure}.{feature}.{bug}
+   *
+   *  structure:用来定义框架的组织结构的更改
+   *  feature:定义对框架的功能上的拓展
+   *  bug:修复框架原有的bug
+   *
+   *  支持String类型的传入。默认值 type:'bug'
+   *
+   *  例如：当前版本号：0.0.1，version.autoIncrement:1
+   *    当type:structure时，version:1.0.0
+   *    当type:feature时:0.1.0
+   *    当type:bug时，version:0.0.2
+   *
+   **/
+
+  releaseConfig: {
+    version: {},
+    type: ""
+  },
+
+  /*
+   * name 包或发布名 (必填)
+   * description 描述 default:undefined
+   * main 入口配置 default:undefined
+   * repository git远程仓库（必填）
+   * dependencies 生产环境依赖 default:undefined
+   * devDependencies 开发环境依赖 default:undefined
+   */
+  packageConfig: {
+    name: "",
+    description: "",
+    main: "",
+    repository: ""
   }
 };
